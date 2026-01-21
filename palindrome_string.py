5e33ef4cb3d7d0addd040811ba60386c0cfdbdb1
Ankita Bhatt <ankitabhatt0313@gmail.com>
2026-01-20 19:23:53 -0600

    check if string is a palindrome

---
+++ palindrome_string.py
@@ -0,0 +1,14 @@
+s="maddiam"
+len=len(s)
+palindrome=True
+print(len)
+
+# Compare first half with second half
+for i in range(len//2):
+    # compare character from start and end
+    if s[i]!=s[len-i-1]:
+        palindrome=False
+        # exit loop if mismatch found
+        break
+print(palindrome)
+    
